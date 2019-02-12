656391985e9d1400f3aacf8f7f6ec14da2b7812d
thondery <thondery@163.com>
2019-02-12 16:46:18 +0800

    add layout account ...

---
+++ layouts/account.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="layout-account">
+    <header>
+      <a class="logo">
+        <img src="~/assets/images/logo.png" />
+      </a>
+      <div class="list">
+        <nuxt-link to="/login">登录</nuxt-link>
+        <nuxt-link to="/">首页</nuxt-link>
+      </div>
+    </header>
+    <div class="bodyer">
+      <nuxt class="account-page" />
+    </div>
+    <footer>
+      Copyright © 2018 Kenote 实验室出品
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component'
+import { Vue } from 'vue-property-decorator'
+import '~/assets/scss/account/layout.scss'
+import '~/assets/scss/account/page.scss'
+
+@Component
+export default class  extends Vue {
+
+  head () {
+    return {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,49 @@
+<template>
+  <page v-if="status === 'invitation'">
+
+  </page>
+  <page v-else-if="status === 'submitinfo'">
+
+  </page>
+  <page v-else-if="status === 'finished'">
+
+  </page>
+</template>
+
+<script lang="ts">
+import Component from 'nuxt-class-component'
+import { Provide, Vue } from 'vue-property-decorator'
+import { namespace } from 'vuex-class'
+import { BindingHelpers } from 'vuex-class/lib/bindings'
+import * as setting from '~/store/modules/setting'
+import { Register } from '~/server/types/config'
+
+const Setting: BindingHelpers = namespace(setting.name)
+
+type Status = 'invitation' | 'submitinfo' | 'validate' | 'finished'
+
+@Component({
+  layout: 'account',
+  mounted () {
+    let { invitation } = <Register> this.register
+    this.$data.status = <Status> (invitation ? 'invitation' : 'submitinfo')
+  }
+})
+export default class  extends Vue {
+
+  @Setting.State register: Register
+
+  @Provide() status: Status = 'invitation'
+  @Provide() invitation?: string
+
+  handleInvitation (value: string): void {
+
+  }
+
+  handleSubmit (value: any): void {
+
+  }
+
+}
+
+</script>
